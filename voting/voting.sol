d9338c67bcafd4aac34ea36e79538c095da0ff6a
Hiroshi Ito <itohiro73@gmail.com>
2018-02-28 16:51:26 +0900

    Add voting example.

---
+++ voting/voting.sol
@@ -0,0 +1,28 @@
+pragma slidity ^0.4.11;
+
+// https://www.youtube.com/watch?v=gSQXq2_j-mw
+contract Voting {
+mapping(bytes32 => uint8) public votesReceived
+bytes32[] public candidateList
+
+function Voting(bytes32[] candidateNames) {
+    candidateList = candidateNames
+}
+
+function totalVotesFor(bytes32 candidate) returns (uint8) {
+    return votesReceived[candidate]
+}
+
+function voteForCandidate(bytes32 candidate) {
+    if(validCandidate(candidate) == false) throw;
+    votesReceived[candidate] += 1;
+}
+
+function validCandidate (byte32 candidate) returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+        if(candidateList[i] == candidate) {
+            return true;
+        }
+    }
+}
+}
